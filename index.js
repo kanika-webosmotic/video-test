1ad291c9149c79e99cc8ff625ef11168ba3b740e
WebOsmotic M1 <webosmoticdeveloper@WebOsmotics-Mac-mini-3.local>
2022-07-27 14:00:36 +0530

    port change

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/video.mp4', function (req, res) {
-  console.log('got video', req.data);
+  console.log('got video', req);
   // Ensure there is a range given for the video
   const range = req.headers.range || '0-';
   if (!range) {
